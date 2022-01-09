19bd30f56cf6f2414d23376165f770f61fa23f3f
s17476 <s17476@pjwstk.edu.pl>
2022-01-10 00:33:55 +0100

    backup

--- lib/providers/checklist_provider.dart
+++ lib/providers/checklist_provider.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:under_control_flutter/models/app_user.dart';
